67e2034ae5698379e292d7777f6569c5d76f748a
sohamdequantzent <sohamsdays@gmail.com>
2023-05-05 16:01:50 +0530

    fix: added confirmation page

--- app/user/order-history/page.tsx
+++ app/user/order-history/page.tsx
@@ -1,10 +1,6 @@
+"use client";
 import OrderHistory from "@/components/User/Order/OrderHistory";
 
 export default function OrderHistoryPage() {
-  return (
-    
-      
-      <OrderHistory />
-    
-  );
+  return <OrderHistory />;
 }
